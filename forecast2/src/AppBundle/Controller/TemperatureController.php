02ca9d27731643788d6f2e879501da3e35c2bbba
jgamesv <jorgevalenciaabad18@gmail.com>
2021-11-16 19:58:16 +0100

    Commit

--- forecast2/src/AppBundle/Controller/TemperatureController.php
+++ forecast2/src/AppBundle/Controller/TemperatureController.php
@@ -2,8 +2,9 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TemperatureController extends Controller
 {
@@ -13,5 +14,13 @@ class TemperatureController extends Controller
     }else {
             return new Response("Register". $id);
         }
+    }
+
+    public function checAction (Request $request){
+        $session = $request->getSession();
+        $log = $session -> get ('log',array());
+        array_push($log,'Temperature checked at'. date ('l jS \of F Y h:i:s A'));
+        $session -> set ('log',$log);
+        return new Response('Temperature cheched alnd log peristed succesfully');
     }
 }
