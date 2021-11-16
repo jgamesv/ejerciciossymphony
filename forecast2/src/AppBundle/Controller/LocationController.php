b3f3d423f4f14d4900dc1764da70498c9e2de9de
jgamesv <jorgevalenciaabad18@gmail.com>
2021-11-16 21:31:48 +0100

    Commit

--- forecast2/src/AppBundle/Controller/LocationController.php
+++ forecast2/src/AppBundle/Controller/LocationController.php
@@ -3,11 +3,17 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class LocationController extends Controller
 {
     public function indexAction () {
         $this -> addFlash ('location','Your are located in Valladolid, Spain');
         return $this -> render('location/index.html.twig');
+    }
+    public function indexJsonAction (){
+        $response = new Response(json_encode(array('location'=> 'Tu estas localizado en Valladolid, Spain')));
+        $response ->headers -> set('Content-type','text/json',true); //'application/json');
+        return $response;
     }
 }
